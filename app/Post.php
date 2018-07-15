c0e9bdbe9da68516d39ac140602b2792d2b7ef56
Vishnu <vishnu.kraman89.outlook.com>
2018-07-16 00:08:18 +0530

    softDeletes

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeltes;
+
     //To Remove this error o fillable property to allow mass assignment on
     protected $fillable =[
         'title','content','category_id','featured'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function category(){
         return $this->belongsTo('App/Category');
