48d125dd88c037de00c5019bf40140f1a9c63b2b
Vishnu <vishnu.kraman89.outlook.com>
2018-07-22 20:43:41 +0530

    Profile Table And Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-            'name' => 'viu',
-            'email' => 'viu@me.com',
-            'password' => bcrypt('vishnu')
+      $user =   App\User::create([
+                'name' => 'viu',
+                'email' => 'viu@me.com',
+                'password' => bcrypt('vishnu'),
+                'admin' => 1
+            ]);
+
+        App\Profile::create([
+            'user_id' => $user->id,
+            'avatar' => 'uploads/avatar/head_deep_blue.png',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fuga blanditiis atque odio soluta, architecto ullam incidunt alias consequatur quia maiores explicabo illo obcaecati, officiis suscipit voluptatibus quis exercitationem sed!',
+            'facebook' => 'facebook.com',
+            'youtube' => 'youtube.com'
         ]);
     }
 }
