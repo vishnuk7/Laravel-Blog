181b02a2bdb4adbe6c8da6e21150ce1a37fe7c27
Vishnu <vishnu.kraman89.outlook.com>
2018-08-23 00:07:34 +0530

    Dynamic content 3

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Setting;
 use App\Post;
 use App\Category;
+
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
@@ -15,6 +16,9 @@ class FrontEndController extends Controller
                 ->with('categories',Category::take(4)->get())
                 ->with('first_post',Post::orderBy('created_at','dsec')->first())
                 ->with('second_post',Post::orderBy('created_at','dsec')->skip(1)->take(1)->get()->first())
-                ->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(2)->get()->first());
+                ->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(2)->get()->first())
+                ->with('development',Category::find(1))
+                ->with('mobileDevelopment',Category::find(4))
+                ->with('setting',Setting::first());
     }
 }
